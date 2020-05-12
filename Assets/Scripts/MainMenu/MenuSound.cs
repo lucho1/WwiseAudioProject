9db25d025fb2766b3cceebac1aba09eecd4027fa
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-05-12 19:23:24 +0200

    Main menu music with Hero and Good lvls done
    Added the main menu music
    
    Co-Authored-By: lucho1 <lucho1@users.noreply.github.com>
    Co-Authored-By: Andres Saladrigas <thearzhel@users.noreply.github.com>
    Co-Authored-By: Roger Leon Borras <rleonborras@users.noreply.github.com>
    Co-Authored-By: Joan Marín <x0ka@users.noreply.github.com>

---
+++ Assets/Scripts/MainMenu/MenuSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour
+{
+    AudioSource Main_menu_music;
+    public AudioClip LoopClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main_menu_music = gameObject.GetComponent<AudioSource>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Main_menu_music.isPlaying==false)
+        {
+            Main_menu_music.clip = LoopClip;
+            Main_menu_music.loop = true;
+            Main_menu_music.Play();
+
+
+        }
+
+
+
+    }
+}
