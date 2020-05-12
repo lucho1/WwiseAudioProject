837ba2c384bfb6544bc7547dab77c669b210e1ed
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-05-12 19:47:56 +0200

    Credits music added
    Co-Authored-By: lucho1 <lucho1@users.noreply.github.com>
    Co-Authored-By: Andres Saladrigas <thearzhel@users.noreply.github.com>
    Co-Authored-By: Roger Leon Borras <rleonborras@users.noreply.github.com>
    Co-Authored-By: Joan Marín <x0ka@users.noreply.github.com>

---
+++ Assets/Scripts/Credits/CreditsMusic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class CreditsMusic : MonoBehaviour
+{
+    AudioSource Credit_music;
+    public AudioClip LoopClip;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Credit_music = gameObject.GetComponent<AudioSource>();
+        Debug.Log("Scene loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Credit_music.isPlaying == false)
+        {
+            Credit_music.clip = LoopClip;
+            Credit_music.loop = true;
+            Credit_music.Play();
+
+            
+        }
+
+
+
+    }
+
+    //void OnSceneLoaded(Scene scene,LoadSceneMode mode)
+    //{
+
+    //    Debug.Log("On Scene loaded:"+scene.name);
+    //    Debug.Log("Mode:"+mode);
+
+    //}
+
+
+
+}
